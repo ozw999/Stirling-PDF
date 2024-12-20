5267bc57c73bcf45a68998d5e6df1f41a4c2c391
obbo <ozw999@gmail.com>
2024-12-20 19:56:23 +0800

    使用动态端口

--- src/main/java/stirling/software/SPDF/BrowserLauncher.java
+++ src/main/java/stirling/software/SPDF/BrowserLauncher.java
@@ -3,29 +3,43 @@ package stirling.software.SPDF;
 import java.awt.*;
 import java.net.URI;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author Ou
  * @date 2024/12/19
  */
 @Component
+@Slf4j
 public class BrowserLauncher implements ApplicationListener<ApplicationReadyEvent> {
+
+    @Autowired private SPdfApplication application;
+
+    @Autowired private Environment environment;
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
         try {
             // 指定要打开的 URL
-            String url = "http://localhost:8080/?lang=zh_CN";
+            String url =
+                    application.getNonStaticBaseUrl()
+                            + ":"
+                            //                            + application.getNonStaticPort()
+                            + environment.getProperty("local.server.port")
+                            + "?lang=zh_CN";
             // 检查是否支持自动打开浏览器
             if (Desktop.isDesktopSupported()) {
                 Desktop desktop = Desktop.getDesktop();
                 desktop.browse(new URI(url));
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("Failed to open browse", e);
         }
     }
 }
